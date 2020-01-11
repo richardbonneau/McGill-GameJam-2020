9c1641923116794637d03c00a3fc49772eac6a51
richardbonneau <richardbonneau@hotmail.com>
2020-01-10 20:24:48 -0500

    basic tile detection on the lefT

--- Assets/_CantGoBack/Scripts/GridHighlight.cs
+++ Assets/_CantGoBack/Scripts/GridHighlight.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class GridHighlight : MonoBehaviour
 {
+    public GameObject leftSelector;
+    public GameObject rightSelector;
+    public GameObject topSelector;
+    public GameObject bottomSelector;
+    public GameObject tile;
     void Start()
     {
         Renderer renderer = this.GetComponent<Renderer>();
         GetComponent<Renderer>().material.SetColor("_Color", Color.red);
     }
+    void LateUpdate()
+    {
+
+
+        if (tile == null)
+        {
+            tile = leftSelector.GetComponent<SelectorCube>().tile;
+            if (tile == null)
+            {
+                tile = new GameObject("");
+                print(tile);
+            }
+
+        }
+    }
+    void onTriggerEnter(Collider col)
+    {
+        print("col " + col);
+    }
     void OnMouseOver()
     {
         GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        // leftSelector.material.SetColor("_Color", Color.blue);
     }
     void OnMouseExit()
     {
---
+++ Assets/_CantGoBack/Scripts/SelectorCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorCube : MonoBehaviour
+{
+    public GameObject tile;
+    void OnTriggerEnter(Collider col)
+    {
+
+        tile = col.gameObject;
+    }
+}
