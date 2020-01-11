391da0ff48e31767c8f352341d05fd235bcb1a91
richardbonneau <richardbonneau@hotmail.com>
2020-01-10 21:13:04 -0500

    two different shapes

--- Assets/_CantGoBack/Scripts/GridHighlight.cs
+++ Assets/_CantGoBack/Scripts/GridHighlight.cs
@@ -9,10 +9,20 @@ public class GridHighlight : MonoBehaviour
     public GameObject rightSelector;
     public GameObject topSelector;
     public GameObject bottomSelector;
+    public GameObject topLeftSelector;
+    public GameObject topRightSelector;
+    public GameObject bottomLeftSelector;
+    public GameObject bottomRightSelector;
     public GameObject leftTile;
     public GameObject rightTile;
     public GameObject topTile;
     public GameObject bottomTile;
+    public GameObject topLeftTile;
+    public GameObject topRightTile;
+    public GameObject bottomLeftTile;
+    public GameObject bottomRightTile;
+
+    public string currentBuildingToPlace = "Farm";
 
     void Start()
     {
@@ -21,13 +31,43 @@ public class GridHighlight : MonoBehaviour
     }
     void LateUpdate()
     {
+        // Initialization
         if (leftTile == null) leftTile = leftSelector.GetComponent<SelectorCube>().tile;
         if (rightTile == null) rightTile = rightSelector.GetComponent<SelectorCube>().tile;
         if (topTile == null) topTile = topSelector.GetComponent<SelectorCube>().tile;
         if (bottomTile == null) bottomTile = bottomSelector.GetComponent<SelectorCube>().tile;
-
+        if (topLeftTile == null) topLeftTile = topLeftSelector.GetComponent<SelectorCube>().tile;
+        if (topRightTile == null) topRightTile = topRightSelector.GetComponent<SelectorCube>().tile;
+        if (bottomLeftTile == null) bottomLeftTile = bottomLeftSelector.GetComponent<SelectorCube>().tile;
+        if (bottomRightTile == null) bottomRightTile = bottomRightSelector.GetComponent<SelectorCube>().tile;
     }
     void OnMouseOver()
+    {
+        switch (currentBuildingToPlace)
+        {
+            case "Mill":
+                MillPlacementEnter();
+                break;
+            case "Farm":
+                FarmPlacementEnter();
+                break;
+        }
+
+    }
+    void OnMouseExit()
+    {
+        switch (currentBuildingToPlace)
+        {
+            case "Mill":
+                MillPlacementExit();
+                break;
+            case "Farm":
+                FarmPlacementExit();
+                break;
+        }
+
+    }
+    void MillPlacementEnter()
     {
         GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
         if (leftTile != null) leftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
@@ -35,13 +75,32 @@ public class GridHighlight : MonoBehaviour
         if (topTile != null) topTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
         if (bottomTile != null) bottomTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
     }
-    void OnMouseExit()
+    void MillPlacementExit()
     {
         GetComponent<Renderer>().material.SetColor("_Color", Color.red);
         if (leftTile != null) leftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
         if (rightTile != null) rightTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
         if (topTile != null) topTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
         if (bottomTile != null) bottomTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
+    void FarmPlacementEnter()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        if (topTile != null) topTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        if (bottomTile != null) bottomTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        if (leftTile != null) leftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        if (topLeftTile != null) topLeftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+        if (bottomLeftTile != null) bottomLeftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+
+    }
+    void FarmPlacementExit()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        if (topTile != null) topTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        if (bottomTile != null) bottomTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        if (leftTile != null) leftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        if (topLeftTile != null) topLeftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+        if (bottomLeftTile != null) bottomLeftTile.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
     }
 
 }
