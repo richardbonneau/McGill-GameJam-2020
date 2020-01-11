8f73fe9b03516daccf0ea373cdf7104a6adfc3d3
richardbonneau <richardbonneau@hotmail.com>
2020-01-10 19:06:24 -0500

    before programming instructions

---
+++ Assets/_CantGoBack/Scripts/GridHighlight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHighlight : MonoBehaviour
+{
+    void Start()
+    {
+        Renderer renderer = this.GetComponent<Renderer>();
+        GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
+    void OnMouseOver()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+    }
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+    }
+
+}
