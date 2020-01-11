144237d4adeab62c5331930cdb815ead4f43576a
richardbonneau <richardbonneau@hotmail.com>
2020-01-11 12:57:16 -0500

    timer bar

--- Assets/_CantGoBack/Scripts/GameManager.cs
+++ Assets/_CantGoBack/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,7 +13,11 @@ public class GameManager : MonoBehaviour
     TextMeshProUGUI currentBuildingUI;
     string nextBuilding = "";
     public GameObject nextBuildingUIGameObject;
+    public Slider slider;
     TextMeshProUGUI nextBuildingUI;
+    float waitTimeUntilNextBuilding = 2f;
+    float elapsedTime = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +26,27 @@ public class GameManager : MonoBehaviour
         nextBuildingUI = nextBuildingUIGameObject.GetComponent<TextMeshProUGUI>();
         buildingTypes[0] = "Farm";
         buildingTypes[1] = "Mill";
-        StartCoroutine(cycleBuildings());
+        StartCoroutine(cycleBuildings(waitTimeUntilNextBuilding));
     }
 
-    IEnumerator cycleBuildings()
+    IEnumerator cycleBuildings(float time)
     {
         while (true)
         {
-            string building = nextBuilding;
+            print(elapsedTime);
+            elapsedTime += 0.05f;
+            slider.value = elapsedTime / time;
+            if (slider.value == 1f)
+            {
+                elapsedTime = 0f;
+                string building = nextBuilding;
+                nextBuilding = buildingTypes[Random.Range(0, 2)];
+                currentBuilding = building;
+                currentBuildingUI.text = building;
+                nextBuildingUI.text = nextBuilding;
+            }
 
-            nextBuilding = buildingTypes[Random.Range(0, 2)];
-            currentBuilding = building;
-            currentBuildingUI.text = building;
-            nextBuildingUI.text = nextBuilding;
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(0.05f);
         }
     }
 }
